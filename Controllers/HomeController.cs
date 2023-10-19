5d6616e0e7f97005bad63e1253ec466f7a900fc2
Leonardo Rios Silva <leonardorios.2009@hotmail.com>
2023-10-19 15:49:15 -0300

    Pagina de consulta só para usuarios logados e tela de consulta holerite (Home, index)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using SystemPag.Models;
 
 namespace SystemPag.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
        
